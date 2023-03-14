928cfdb0d7df41d2b06b8224c454d9bf07db79b3
Jeferson Santos Moraes de Souza <406159@unoesc.lan>
2023-03-13 22:01:27 -0300

    update thousands

--- ex-01/Program.cs
+++ ex-01/Program.cs
@@ -9,22 +9,35 @@
         return firstNumbers[absoluteNumber];
     }
 
-    for (int i = 0; i < number.Length - 1; i++)
+    if (number.Length < 4)
     {
-        string[] tens = { "", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa" };
-        string[] hundreds = { "cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos" };
-        string[] thousands = { "mil", "dois mil", "três mil", "quatro mil", "cinco mil", "seis mil", "sete mil", "oito mil", "nove mil" };
-        string[][] numberSets = { tens, hundreds, thousands };
+        for (int i = 0; i < number.Length - 1; i++)
+        {
+            string[] tens = { "", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa" };
+            string[] hundreds = { "cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos" };
+            string[][] numberSets = { tens, hundreds };
 
-        int digit = number.Length - i;
-        string currentText = numberSets[digit - 2][int.Parse(number[i].ToString()) - 1];
-        int rest = int.Parse(number.ToString().Remove(0, 1));
-        string connector = digit < 4 ? " e " : " ";
-        string nextNumber = rest > 0 ? connector + NumberToText(rest) : " ";
-        return currentText + nextNumber;
+            int digit = number.Length - i;
+            string currentText = numberSets[digit - 2][int.Parse(number[i].ToString()) - 1];
+            int rest = int.Parse(number.ToString().Remove(0, 1));
+            string connector = digit < 4 ? " e " : " ";
+            string nextNumber = rest > 0 ? connector + NumberToText(rest) : " ";
+            return currentText + nextNumber;
+        }
+    }
+
+    if (number.Length < 7) {
+        Console.WriteLine(number );
+        for (int i = 0; i < 4; i++) {
+            int digit = number.Length - i;
+            string thousands = int.Parse(number[i].ToString()) > 1 ? NumberToText(int.Parse(number[i].ToString())) + " mil " : "mil ";
+            int rest = int.Parse(number.ToString().Remove(0, 1));
+            return thousands + NumberToText(rest);
+        }
     }
 
     return "";
+
 }
 
 
